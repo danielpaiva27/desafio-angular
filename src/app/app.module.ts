c756f444a270b1d4c80fa9ef524bdbe2dbc32b25
Daniel Luna Paiva <daniel.00000853754@unicap.br>
2024-07-31 11:56:52 -0300

    container de login adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {HttpClientModule} from  '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component/first-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { FirstComponentComponent } from './components/first-component/first-comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/first-component/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component/first-component.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.scss']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
 
+
+  constructor() {}
+
+  ngOnInit(): void{
+
+  }
 }
+
